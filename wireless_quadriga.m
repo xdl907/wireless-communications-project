f3be5ecea03d007d305cb6589fa22b70316dbda4
𝚡𝚍𝚕 <bobbyrms97@gmail.com>
2019-12-07 11:34:04 +0000

    added a little comments and OFDM signal generation up to QAM modulation

--- wireless_quadriga.m
+++ wireless_quadriga.m
@@ -1,3 +1,4 @@
+clear, close all;
 %% Random coordinate generation
 g.bs = [0,0,25]; %base station
 g.t1 = [-50 + rand(2,1)*100;1.5]; %terminal 1
@@ -22,7 +23,7 @@ array_rx.no_elements = 16;
 array_rx.element_position=g.bs_antenna_pos; %fetching the URA antenna positions for the quadriga layout
 
 %% Quadriga channel model
-l.tx_array = array_tx;
+l.tx_array = array_tx; %array definitions
 l.rx_array = array_rx;
 l.no_rx = 1;
 l.no_tx = 4;
@@ -31,27 +32,36 @@ tx_track2= qd_track('linear',20,pi/2); %track of the 2nd terminal (add random va
 tx_track1.name = 'UE1';
 tx_track2.name = 'UE2';
 
-l.tx_track(1,1) = copy(tx_track1);
+l.tx_track(1,1) = copy(tx_track1); %track establishment
 l.tx_track(1,2) = copy(tx_track2);
-l.rx_position = g.bs';
+l.rx_position = g.bs'; %position fetching
 l.tx_position=[g.t1,g.t2,g.i1,g.i2];
 l.visualize();
-l.set_pairing;
+l.set_pairing; %pair all tx/rx links
 chan = l.get_channels(); %channel generation
 
 %to be continued...
 
-%% OFDM signal generation
-% We emulate a LTE Cat.16 OFDM DL transmission with the following parameters
+%% OFDM signal generation (LTE-A)
 
-M = 64; %64-QAM
+%QAM signal generation
+M = 64; %modulation order
 k = log2(M); %bits per symbol
+tx_symbols = randi([0 M-1],2000,1);
+refc = qammod(0:M-1,M);
+mod_qam = qammod(tx_symbols, M, 'gray');
+%add encoding in the future? ask reggiani
+constdiag = comm.ConstellationDiagram('ReferenceConstellation',refc, ...
+    'XLimits',[-9 9],'YLimits',[-9 9]);
+constdiag(mod_qam);
+rcv = awgn(mod_qam,10); %AWGN channel for testing
+constdiag(rcv);
+
+%OFDM parameters
 n_subc = 2048; %subcarriers
 cp_len = n_subc*0.25; %with long cyclic prefix in LTE, the length is 25% of the total symbol rate (??? da verificare)
 
-tx_bits = randi([0,1],1000);
-mod_qam = qammod(tx_bits, M, 'gray');
-%add encoding in the future? ask reggiani
-ofdmMod = comm.OFDMModulator('FFTLength',n_subc,'CyclicPrefixLength',cp_len); %ofdm modulator object
+
+
 
 
